a05c2dab3f7dc611e7d7485cf498d231c66cb690
Nial <48334675+nmcc1212@users.noreply.github.com>
2026-02-06 10:32:59 +0000

    add TEXT_VALUE environment variable and update Home component to use it

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { unstable_noStore } from "next/cache";
 
 export default function Home() {
+  unstable_noStore();
+  const textValue = process.env.TEXT_VALUE || "No Value";
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,7 +17,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Hello World - {process.env.NEXT_PUBLIC_TEXT}
+            Hello World - {textValue}
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Welcome to your Next.js application!
